6db72d1dfafa8ccab736a207798966abfe800f48
geronimo maspero <geronimomaspero@gmail.com>
2021-05-02 22:04:23 -0300

    finally finished warm up excercise. We created a fancy, good looking biplot map

--- warmUpExercise/main.py
+++ warmUpExercise/main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
-
+from sklearn.decomposition import PCA
+from sklearn import datasets
+import plotly.express as px
 
 
 #------------------ data parsing ------------------#
@@ -19,6 +22,14 @@ parsed_file = pd.read_csv('europe.csv')
 
 #------------------ data parsing ------------------#
 
+
+
+
+columns_names_list = parsed_file.columns.tolist()
+columns_names_list.pop(0)
+
+# print(columns_names_list)
+# ['Area', 'GDP', 'Inflation', 'Life.expect', 'Military', 'Pop.growth', 'Unemployment']
 
 
 #------------------ list of countries ------------------#
@@ -90,20 +101,14 @@ scaled_properties_values = scaler.fit_transform(properties_values)
 #------------------ covariance matrix ------------------#
 
 df = pd.DataFrame(scaled_properties_values)
-
 covariance_matrix = pd.DataFrame.cov(df)
 
-print(covariance_matrix)
+#print(covariance_matrix)
 
 
-# print("-----------------------------------------")
-
-data = np.array(scaled_properties_values)
-
-covariance_matrix2 = np.cov(scaled_properties_values.T) 
-
-print(covariance_matrix2)
-
+# Alternate way to find covariance matrix
+#covariance_matrix2 = np.cov(scaled_properties_values.T) 
+#print(covariance_matrix2)
 
 #------------------ covariance matrix ------------------#
 
@@ -117,10 +122,7 @@ w, v = np.linalg.eig(covariance_matrix)
 w = sorted(w, reverse=True) 
 
 #print(w)
-
 # [3.3466903338935152, 1.2310909419122338, 1.1025679615273636, 0.798887683362183, 0.47480597493684956, 0.17492107429275486, 0.1302952893343626]
-
-
 
 
 #------------------ eigenvalues (w) & eigenvectors (v) ------------------#
@@ -128,5 +130,73 @@ w = sorted(w, reverse=True)
 
 
 
+
+
+#------------------ PCA Calculation ------------------#
+
+pca = PCA(n_components=7).fit(scaled_properties_values)
+
+# pca.explained_variance_ratio_: Percentage of variance explained by each of the selected components.
+
+# print(pca.explained_variance_ratio_)
+# [0.46102367 0.16958906 0.15188436 0.11005085 0.06540695 0.02409627 0.01794884]
+
+
+# print(np.cumsum(pca.explained_variance_ratio_))
+# [0.46102367 0.63061273 0.78249709 0.89254794 0.95795489 0.98205116 1.00]
+
+#plt.plot(np.cumsum(pca.explained_variance_ratio_))
+#plt.show()
+
+
+#print charges! 
+#print(pca.components_)
+#[
+# [ 1.24873902e-01 -5.00505858e-01  4.06518155e-01 -4.82873325e-01  1.88111616e-01 -4.75703554e-01  2.71655820e-01] --> autovector A
+# [-1.72872202e-01 -1.30139553e-01 -3.69657243e-01  2.65247797e-01  6.58266888e-01  8.26219831e-02  5.53203705e-01] --> autovector B
+# ...... ]
+
+
+#EJERCICIO: INTERPRETAR LA PRIMERA COMPONENTE
+#Si la componente principal del dataset me da negativo (ex: -2), el autovector A, va a tener mas de de las componentes 
+# negativas (mas de "-5.00505858e-01", de "-4.82873325e-01" y de "-4.75703554e-01"
+
+
+#------------------ PCA Calculation ------------------#
+
+
+
+pca2 = PCA(n_components=7)
+
+components = pca2.fit_transform(scaled_properties_values)
+
+
+loadings = pca2.components_.T * np.sqrt(pca2.explained_variance_)
+
+
+#dots ploted in the figure
+# Value of 'x' is not the name of a column in 'data_frame'. Expected one of [0, 1, 2, 3, 4, 5, 6]
+# Value of 'y' is not the name of a column in 'data_frame'. Expected one of [0, 1, 2, 3, 4, 5, 6]
+fig = px.scatter(components, x=0, y=1, color=countries_list_values)
+
+
+#we add the vectors to the figure
+for i, feature in enumerate(columns_names_list):
+    fig.add_shape(
+        type='line',
+        x0=0, y0=0,
+        x1=loadings[i, 0],
+        y1=loadings[i, 1]
+    )
+    fig.add_annotation(
+        x=loadings[i, 0],
+        y=loadings[i, 1],
+        ax=0, ay=0,
+        xanchor="center",
+        yanchor="bottom",
+        text=feature,
+    )
+
+fig.show()
 
 
